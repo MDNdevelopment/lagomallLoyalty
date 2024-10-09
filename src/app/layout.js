b05a37bd4204fbe67a17bb63fcba9333e8d31e39
MDNdevelopment <transferenciasmdn@gmail.com>
2024-10-09 16:34:00 -0400

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lago Mall",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es" translate="no">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
